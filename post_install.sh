03a652b05cccd4df431efa0abddcf962e9e41ce8
fulder <misad90@gmail.com>
2020-04-09 22:22:09 +0200

    Start up mysql server and create php.ini

--- post_install.sh
+++ post_install.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
 
+echo "Enable and start Apache"
 sysrc apache24_enable=YES
 service apache24 start
+
+echo "Enable and start mysql server"
+sysrc mysql_enable=YES
+service mysql-server start
+
+echo "Create php.ini"
+cp /usr/local/etc/php.ini-production /usr/local/etc/php.ini
